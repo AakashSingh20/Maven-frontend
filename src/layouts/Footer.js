aadea4a5cc402e11a9bbc44c9d65eea1cb0e6198
AakashSingh20 <akashsingh20603@gmail.com>
2024-04-20 09:17:52 +0530

    footer changes

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -2,50 +2,50 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4 w-full ">
-      <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div class="sm:flex sm:items-center sm:justify-between">
+    <footer className="w-full bg-white rounded-lg shadow dark:bg-gray-900 ">
+      <div className="w-full max-w-screen-xl p-4 mx-auto md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
           <a
-            href="https://flowbite.com/"
-            class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+            href="#"
+            className="flex items-center mb-4 space-x-3 sm:mb-0 rtl:space-x-reverse"
           >
             <img
               src="https://flowbite.com/docs/images/logo.svg"
-              class="h-8"
+              className="h-8"
               alt="Flowbite Logo"
             />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               Maven
             </span>
           </a>
-          <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
-              <a href="#" class="hover:underline me-4 md:me-6">
+              <a href="#" className="hover:underline me-4 md:me-6">
                 About
               </a>
             </li>
             <li>
-              <a href="#" class="hover:underline me-4 md:me-6">
+              <a href="#" className="hover:underline me-4 md:me-6">
                 Privacy Policy
               </a>
             </li>
             <li>
-              <a href="#" class="hover:underline me-4 md:me-6">
+              <a href="#" className="hover:underline me-4 md:me-6">
                 Licensing
               </a>
             </li>
             <li>
-              <a href="#" class="hover:underline">
+              <a href="#" className="hover:underline">
                 Contact
               </a>
             </li>
           </ul>
         </div>
-        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023{" "}
-          <a href="https://flowbite.com/" class="hover:underline">
-            Maven"
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2023
+          <a href="#" className="hover:underline">
+            Maven
           </a>
           . All Rights Reserved.
         </span>
@@ -53,4 +53,3 @@ export const Footer = () => {
     </footer>
   );
 };
-
